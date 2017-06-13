31e85c72d87fff543c678f705c6043b7e2e4263f
Kaustubh Singh <kaustubh.singh@gmail.com>
2017-06-12 21:33:55 -0400

    update

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
