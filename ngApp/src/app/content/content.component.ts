f9ed4aaad44442c4dd0d64b29257b49c954f1409
Kaustubh Singh <kaustubh.singh@gmail.com>
2017-06-17 09:38:09 -0400

    update

--- ngApp/src/app/content/content.component.ts
+++ ngApp/src/app/content/content.component.ts
@@ -29,16 +29,16 @@ export class ContentComponent implements OnInit {
 Highcharts.chart('container1', {
 
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'Highcharts Line Chart'
     },
 
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+    
     },
 
     yAxis: {
         title: {
-            text: 'Number of Employees'
+            text: 'Values'
         }
     },
     legend: {
@@ -54,19 +54,19 @@ Highcharts.chart('container1', {
     },
 
     series: [{
-        name: 'Installation',
+        name: 'Matric A',
         data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
     }, {
-        name: 'Manufacturing',
+        name: 'Matric B',
         data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
     }, {
-        name: 'Sales & Distribution',
+        name: 'Matric C',
         data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
     }, {
-        name: 'Project Development',
+        name: 'Matric D',
         data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
     }, {
-        name: 'Other',
+        name: 'Matric E',
         data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
     }]
 
