99c378aa4ce85d9a9f57a2088c57ab03e6864ade
Kaustubh Singh <kaustubh.singh@gmail.com>
2017-06-17 00:23:16 -0400

    update

--- ngApp/src/app/services/first.service.ts
+++ ngApp/src/app/services/first.service.ts
@@ -4,20 +4,61 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class FirstService {
-    private searchURL: string;
 
-    constructor(private _http:Http) {
+
+    constructor() {
 
     }
 
-    searchMusic(str:string, type="artist"){
-        this.searchURL = "https://api.spotify.com/v1/search?query="+str+"&offset=0&limit=20&type="+type+"&market=US";
-        return this._http.get(this.searchURL) //, {
-         //   headers: {
-         //       'Authorization': 'Bearer a41f0df10a654e729a54435761b440fd'
-         // }})
-        .map(res => res.json());
+    renderChart(){
 
+        console.log("chart called...");
+
+Highcharts.chart('container1', {
+
+    title: {
+        text: 'Solar Employment Growth by Sector, 2010-2016'
+    },
+
+    subtitle: {
+        text: 'Source: thesolarfoundation.com'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    }]
+
+});
 
     }
 }
